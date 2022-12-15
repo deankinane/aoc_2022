93fad304c372a2fb38a5b2b097df95afe6d3d6f2
Dean Kinane <dean.kinane@gmail.com>
2022-12-15 08:48:20 +0000

    Removed old version of day 8 part 1

--- 8/8.ts
+++ 8/8.ts
@@ -1,63 +1,84 @@
-import {readFileSync} from 'fs'
-
-const text = readFileSync('./input.test').toString()
+import { readFileSync } from 'fs'
+const text = readFileSync('./input.prod').toString()
 
 const lines = text.split('\n')
-lines.splice(lines.length-1, 1) // remove trailing blank line
+lines.splice(lines.length - 1, 1) // remove trailing blank line
 
+const treeGrid = lines.map(c => c.split('').map(x => parseInt(x)))
+const visible: number[][] = []
 
-const treeGrid: number[][] = lines.map(x => x.split('').map(y => parseInt(y)))
+console.log(treeGrid);
 
- console.log('grid', treeGrid)
-
-const visibleGrid: number[][] = []
-
-for (let i=0; i<treeGrid.length; i++){
+for (let i = 0; i < treeGrid.length; i++) {
     const row = treeGrid[i]
-    visibleGrid[i] = []
-
-    for(let j=0; j<row.length; j++) {
-        if(i==0 || i==treeGrid.length-1 || j===0 || j===row.length-1){
-            visibleGrid[i][j] = 1
-            continue
-        } 
-
-        visibleGrid[i][j] = 0
-
-        if(treeGrid[i][j-1] < treeGrid[i][j] && visibleGrid[i][j-1] === 1){
-            visibleGrid[i][j] = 1
+    visible[i] = []
+    for (let j = 0; j < row.length; j++) {
+        visible[i][j] = 0
+        if (i == 0 || i == treeGrid.length - 1 || j == 0 || j == row.length - 1) {
+            visible[i][j] = 1
         }
+
     }
+}
 
-    for(let j=row.length-1; j>=0; j--) {
-        if(i==0 || i==treeGrid.length-1 || j===0 || j===row.length-1){
-            continue
-        } 
-
-        if(treeGrid[i][j+1] < treeGrid[i][j] && visibleGrid[i][j+1] === 1){
-            visibleGrid[i][j] = 1
+for (let i = 1; i < treeGrid.length; i++) {
+    for (let j = 1; j < treeGrid[0].length; j++) {
+        if (castLeft(j, i) || castRight(j, i) || castUp(j, i) || castDown(j, i)) {
+            visible[i][j] = 1
         }
     }
 }
 
-// console.log(visibleGrid)
-
-for (let i=1; i<visibleGrid[0].length; i++){
-
-    for(let j=1; j<visibleGrid.length; j++) {
-        if(treeGrid[j][i] > treeGrid[j-1][i] && visibleGrid[j-1][i] === 1) {
-            treeGrid[j][i] = 1
+function castLeft(startX: number, startY: number): boolean {
+    let x = startX
+    while (x > 0) {
+        x--
+        if (treeGrid[startY][x] >= treeGrid[startY][startX]) {
+            return false
         }
     }
-  
-    for(let j=visibleGrid.length-2; j>=0; j--) {
-        if(treeGrid[j+1][i] < treeGrid[j][i] && visibleGrid[j+1][i] === 1) {
-            treeGrid[j][i] = 1
-        }
-    }
+
+    return true
 }
 
+function castRight(startX: number, startY: number): boolean {
+    let x = startX
+    while (x < treeGrid[0].length - 1) {
+        x++
+        if (treeGrid[startY][x] >= treeGrid[startY][startX]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+function castUp(startX: number, startY: number): boolean {
+    let y = startY
+    while (y > 0) {
+        y--
+        if (treeGrid[y][startX] >= treeGrid[startY][startX]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+function castDown(startX: number, startY: number): boolean {
+    let y = startY
+    while (y < treeGrid.length - 1) {
+        y++
+        if (treeGrid[y][startX] >= treeGrid[startY][startX]) {
+            return false
+        }
+    }
+
+    return true
+}
+console.log(visible);
+
+const result = visible.map(x => x.reduce((i = 0, j) => i + j)).reduce((i = 0, j) => i + j)
+console.log('visible', result);
 
 
- console.log(visibleGrid)
-console.log('Visible trees:', visibleGrid.map(x => x.reduce((i=0, r) => i+r)).reduce((i=0,r) => i+r))
